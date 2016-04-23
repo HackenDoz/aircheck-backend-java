6078f93d169e8dc74be7a9b45093c1b59e92c76b
jimgao <admin@jimgao.me>
2016-04-23 01:59:00 -0400

    fuck

--- src/aircheck/ServerConfig.java
+++ src/aircheck/ServerConfig.java
@@ -9,7 +9,7 @@ public class ServerConfig {
     public static final String MYSQL_HOST = "localhost";
     public static final int MYSQL_PORT = 3306;
     public static final String MYSQL_DB = "aircheck";
-    public static final String MYSQL_USER = "aircheck";
+    public static final String MYSQL_USER = "root";
     public static final String MYSQL_PASS = "AirCheck>SQLserver<123";
 
     public static final int CIRCLE_RADIUS = 10;
