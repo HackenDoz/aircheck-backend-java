4efe66607c64f930c2216ad93583c80865788d7b
jimgao <admin@jimgao.me>
2016-04-22 23:26:20 -0400

    Update

--- src/aircheck/Main.java
+++ src/aircheck/Main.java
@@ -1,13 +1,21 @@
 package aircheck;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
-    private Map<Integer, ArrayList<UserReport>> reports;
+    public static Map<Integer, UserReport> reports;
+    public static DatabaseAdapter dbAdapter;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        reports = new HashMap<>();
+        dbAdapter = new DatabaseAdapter();
 
+        while(true){
+            try{ Thread.sleep(5000); } catch (Exception ex) { }
+            dbAdapter.getReports(reports);
+        }
     }
 }
